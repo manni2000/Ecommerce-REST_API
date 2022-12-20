0d17809bb7ebbe4da36c4f0750e4bc790f4d948e
Manish kumar <91480902+manni2000@users.noreply.github.com>
2022-12-20 13:37:57 +0530

    update

--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-const UserSchema = mongoose.Schema({
+const UserSchema = mongoose.Schema(
+    {
     username: {
         type: String,
         required: true
@@ -40,8 +41,10 @@ const UserSchema = mongoose.Schema({
     country: {
         type: String,
         default: ''
-    }
-})
+    },
+  },
+  { timestamps: true }
+);
 
 UserSchema.virtual('id').get(function () {
     return this._id.toHexString();
